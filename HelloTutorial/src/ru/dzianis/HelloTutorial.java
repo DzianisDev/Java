2e41635888246379f2b5c47f0b44ab28701f0963
Dzianis Kazakevich <kazakevichdev@gmail.com>
2022-03-23 12:06:28 +0300

    HW_1 Println in CLI

---
+++ HelloTutorial/src/ru/dzianis/HelloTutorial.java
@@ -0,0 +1,8 @@
+package ru.dzianis;
+
+public class HelloTutorial {
+    //EPAM Simple CLI Application
+    public static void main(String[] args) {
+	System.out.println("tutorial->https://docs.oracle.com/javase/tutorial/");
+    }
+}
